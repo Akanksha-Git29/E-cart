221e9fc158ae162cacd4997e7e22e68846ca3abe
Akanksha-Git29 <69651538+Akanksha-Git29@users.noreply.github.com>
2021-11-20 16:42:53 +0530

    backend cover

--- server/routes/productsApi.js
+++ server/routes/productsApi.js
@@ -1,8 +1,70 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/authorization')
+const {check,validationResult} = require('express-validator')
+const Product = require('../models/Products')
 
-router.get("/",(req,res)=>{
-    res.send("Product route")
+
+router.post("/",
+[auth,
+    [
+        check("name","Name is Required").not().isEmpty(),
+        check("description","Description is Required").not().isEmpty(),
+        check("category","Category is Required").not().isEmpty(),
+        check("price","Price is Required").not().isEmpty(),
+        check("quantity","Quantity is Required").not().isEmpty()
+    ]
+],
+async (req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const {name,description,category,price,brand,quantity} = req.body
+        const newProduct = new Product({
+            userId: req.user.id,
+            name,
+            description,
+            category,
+            price,
+            brand,
+            quantity
+            })
+        const product = await newProduct.save()
+        res.json({product})
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+    }
+})
+
+
+router.get("/",async (req,res)=>{
+    try {
+        const product = await Product.find()
+        if(!product){
+            return res.status(404).json({msg:"Products not found"})
+        }
+        res.json(product)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+    }
+})
+
+router.get("/:id", async (req,res)=>{
+    try {
+        const product = await Product.findById(req.params.id)
+        if(!product){
+            return res.status(404).json({msg:"Products not found"})
+        }
+        res.json(product)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+    }
 })
 
 module.exports = router
